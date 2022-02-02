47a82742311a7d8d45d8c081439a13ef7c23d77f
Pablo <pablo.velez01@epn.edu.ec>
2022-02-01 19:00:21 -0500

    Inicio

---
+++ Fundamentos/prueba.py
@@ -0,0 +1,14 @@
+#Nuestro primerscript
+'''
+A
+A
+A
+Este es un comentario largo
+'''
+
+#Entreda de datos se realiza con input
+#Salida de datos se realiza con print
+print('Formulario')
+print('Ingresa tu nombre: ')
+nombre = input()
+print('Elnombre ingresado es: ', nombre)
